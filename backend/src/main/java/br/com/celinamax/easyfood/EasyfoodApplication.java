18a9df2b4d4972917651d4ce216368b4b3f483ad
Celina Maximiano Almeida <celinamax01@gmail.com>
2021-01-05 22:08:43 -0300

    Project created

---
+++ backend/src/main/java/br/com/celinamax/easyfood/EasyfoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.celinamax.easyfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyfoodApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/celinamax/easyfood/EasyfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.celinamax.easyfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
